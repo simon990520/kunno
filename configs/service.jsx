02a0d400165245e38a7245a65e4e8c8894c35d0e
simon990520 <simontrinidad.f.b@gmail.com>
2024-12-01 10:10:08 -0500

    Verify error vercel 403

--- configs/service.jsx
+++ configs/service.jsx
@@ -1,20 +1,30 @@
-const { default: axios } = require("axios")
+"use client";
+import axios from "axios";
 
-const YOUTUBE_BASE_URL='https://www.googleapis.com/youtube/v3'
+const YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3";
 
-const getVideos= async(query)=>{
-    const params={
-        part:'snippet',
-        q:query,
-        maxResults:1,
-        type:'video',
-        key:process.env.NEXT_PUBLIC_YOUTUBE_API_KEY
+const getVideos = async (query) => {
+  try {
+    const params = {
+      part: "snippet",
+      q: query,
+      maxResults: 1,
+      type: "video",
+      key: process.env.NEXT_PUBLIC_YOUTUBE_API_KEY, // Asegúrate de que la clave esté configurada en el entorno
+    };
 
-    }
-    const resp =await axios.get(YOUTUBE_BASE_URL+'/search',{params});
+    const response = await axios.get(`${YOUTUBE_BASE_URL}/search`, { params });
+    return response.data.items;
+  } catch (error) {
+    console.error("Error al obtener videos de YouTube:", {
+      status: error.response?.status,
+      data: error.response?.data,
+      message: error.message,
+    });
+    throw error;
+  }
+};
 
-    return resp.data.items;
-}
-export default{
-    getVideos
-}
+export default {
+  getVideos,
+};
